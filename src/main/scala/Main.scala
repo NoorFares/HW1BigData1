3aa57fe7d9ba723de308420612b657386908cd57
git config --global user.namNoorFares <s12028537@stu.najah.edu>
2022-10-12 18:08:49 +0300

    initial comitt

---
+++ src/main/scala/Main.scala
@@ -0,0 +1,46 @@
+
+import org.apache.log4j.BasicConfigurator
+import org.apache.log4j.varia.NullAppender
+import org.apache.spark.sql.expressions.Window
+import org.apache.spark.sql.functions._
+import org.apache.spark.sql.{SparkSession, functions}
+object Main {
+  def main(args: Array[String]): Unit = {
+    val nullAppender = new NullAppender
+    BasicConfigurator.configure(nullAppender)
+    val spark = SparkSession
+      .builder()
+      .master("local")
+      .appName("Spark SQL basic example")
+      .getOrCreate()
+    //the path of folder contains folder
+    val path = "C:\\Users\\Msys\\Desktop\\files\\*";
+    //read all the data From file
+    val rdd = spark.sparkContext.textFile(path)
+    val counts = {
+      rdd.flatMap(line => line.split(" ")).map(word => (word, 1)).reduceByKey(_ + _).foreach(f => {
+        println(f._1 + "=>" + f._2)
+      })
+    }
+    //print the number of col and row
+    val nRows = rdd.count()
+    val data = rdd.map(line => line.split(",").map(elem => elem.trim()))
+    val nCols = data.collect()(0).length
+    println("number of row" + nRows)
+    println("number of col" + nCols)
+
+    //create a Data Frame of the files contain 2 coulmn value and file name
+    val filterFile: String => String = _.split("/").takeRight(1)(0)
+    val filterFileUDF = udf(filterFile);
+    val DataFrame = spark.read.format(path).text(path).withColumn("Filename", (filterFileUDF(input_file_name())
+      )).withColumn("Value", explode(functions.split(column("value"), " "))).orderBy(column("Filename")).withColumn("Filename", collect_list("Filename").over(Window.partitionBy("Value")))
+    //df.orderBy(col("Value"),col("Filename")).show(false)
+    println("the value and where are in fileName")
+    DataFrame.select("Value","Filename").show()
+    DataFrame.select("Value","Filename").orderBy("Value").collect().take(20).foreach(println)
+
+    DataFrame.groupBy("Value","Filename").agg(count("Value").as("Count")).orderBy("Value")
+      .show()
+
+  }
+}
